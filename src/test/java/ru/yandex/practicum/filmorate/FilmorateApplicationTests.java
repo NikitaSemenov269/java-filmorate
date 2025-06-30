e752531f6ce8ee522dc84596ac025cf7ee0f0ee7
Nikita Semenov <nik.269@yandex.ru>
2025-06-30 21:56:19 +0300

    Исправлена ошибка в имени класса

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -20,7 +20,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-class FilmAndUserValidationTest {
+class FilmorateApplicationTests {
 
     @Autowired
     private TestRestTemplate restTemplate;
