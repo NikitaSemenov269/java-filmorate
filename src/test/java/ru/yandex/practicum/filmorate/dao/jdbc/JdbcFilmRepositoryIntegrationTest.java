51ed139f21e21bdf3344aa7b42c311ccd11e5f86
Nikita Semenov <nik.269@yandex.ru>
2025-08-27 09:45:46 +0300

    Исправлены тесты.

--- src/test/java/ru/yandex/practicum/filmorate/dao/jdbc/JdbcFilmRepositoryIntegrationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dao/jdbc/JdbcFilmRepositoryIntegrationTest.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.dao.jdbc;
 
 import org.assertj.core.api.AssertionsForInterfaceTypes;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
